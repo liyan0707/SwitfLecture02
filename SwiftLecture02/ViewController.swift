e6952a3e236448a238981165eb8eb37b61e4126d
liyan0707 <728762713@qq.com>
2018-07-22 10:27:54 +0800

    add name

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello Swift")
+        print("Hello Swift!")
+        var  name : String =  "yan"
+        let  fullName = name + "Li "
+        print(fullName)
+        //
     }
 
     override func didReceiveMemoryWarning() {
